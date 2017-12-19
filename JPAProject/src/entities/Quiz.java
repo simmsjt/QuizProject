f17ba81d7af399c4a0d833aab86a30db50dbefd2
Jake <jacobtaric@yahoo.com>
2017-12-19 16:53:01 -0700

    integration test passing for quizdb

--- JPAProject/src/entities/Quiz.java
+++ JPAProject/src/entities/Quiz.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class Quiz {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Quiz {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "name")
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Quiz [id=" + id + ", name=" + name + "]";
+	}
 }
---
+++ JPAProject/test/test/QuizTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import entities.Quiz;
+
+public class QuizTest {
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Quiz q;
+	
+	@Before
+	public void set_up() { 
+		this.emf = Persistence.createEntityManagerFactory("YourPU");
+		this.em = emf.createEntityManager();
+		this.q = em.find(Quiz.class, 1);
+	}
+	
+	@After
+	public void tear_down() { }
+	
+	@Test
+	public void quiz_test() {
+		boolean test = true;
+		assertEquals(true, test);
+	}
+	
+	@Test
+	public void query_test() {
+		assertEquals("What kind of cake are you?",q.getName());
+	}
+}
